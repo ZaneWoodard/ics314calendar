b86b93b270c7fe6f3b6f64af9f69025dabe54116
ZaneWoodard <zwoodard@hawaii.edu>
2016-02-25 23:26:41 -1000

    Convert to using Dates and a Utililties converter. Additionally, generify the file output path.

--- src/ics314/dezesseis/calendar/CalendarExample.java
+++ src/ics314/dezesseis/calendar/CalendarExample.java
@@ -2,6 +2,7 @@ package ics314.dezesseis.calendar;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
 import java.io.FileOutputStream;
 
 
@@ -10,16 +11,22 @@ public class CalendarExample {
         VObject calendar = new VObject(VObjectType.CALENDAR);
         
         VObject event = new VObject(VObjectType.EVENT);
+        
+        Calendar start = Calendar.getInstance();
+        start.set(2016, 2, 26, 10, 0, 0);
+        Calendar end = Calendar.getInstance();
+        end.set(2016, 2, 26, 10, 15, 0);     
+
         event.addSummary("TA Meeting");
         event.addDescription("Team meeting with the ICS314 TA");
-        event.addDtStart("20160226T103000");
-        event.addDtEnd("20160226T104500");
-        event.addDtStamp("20160224T104500");
+        event.addDtStart(Utilities.dateToDtString(start.getTime()));
+        event.addDtEnd(Utilities.dateToDtString(end.getTime()));
+        event.addDtStamp(Utilities.dateToDtString(Calendar.getInstance().getTime()));
         event.addLocation("ICS Lounge in POST");
         
         calendar.addChild(event);
         
-        File out = new File("/Users/Zane/Desktop/test.ics");
+        File out = new File("test.ics");
         try(FileOutputStream outStream = new FileOutputStream(out)) {
             outStream.write(calendar.getTextRepresentation().getBytes());
             outStream.close();
