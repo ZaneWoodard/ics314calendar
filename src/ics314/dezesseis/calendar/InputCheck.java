81b9bfb50ae3e58e402c137e2df67a49386ce3bc
niko925 <niko2012925@gmail.com>
2016-02-25 19:49:11 -1000

    Added files via upload

---
+++ src/ics314/dezesseis/calendar/InputCheck.java
@@ -0,0 +1,125 @@
+package ics314.dezesseis.calendar;
+import java.util.Scanner;
+
+public class InputCheck {
+	Scanner userInput =new Scanner(System.in);
+
+	public String Checktime(String BETime){
+		String input = "";
+		boolean again = false;
+		//loop until user input a correct time
+		do{
+			System.out.print("Please, enter the "+ BETime +" time. e.g.(14:20): ");
+			input = userInput.nextLine();
+			input = input.trim();
+			if(input.substring(2, 3).equals(":") && input.length() == 5){
+				try {
+					//check the time is or not integer
+					int num1 = Integer.parseInt(input.substring(0, 2));
+					int num2 = Integer.parseInt(input.substring(3, 5));
+					//if 
+					if(num1<0  || num1 >24 || num2<0 || num2>60){
+						again = true;
+						System.out.println("Error:only has 24 hours and 60 minutes");
+					}
+					else{
+						again = false;
+					}
+				}catch (NumberFormatException e) {
+					again = true;
+					System.out.println("Error: Please,enter number.");
+				}
+			}
+			else{
+				again = true;
+				System.out.println("Please,enter a correct time.");
+			}
+		}while(again);
+		input = input.replace(":","");
+		input = input.trim();
+		return input;
+	}
+	
+	public String CheckDate(String BEDay){
+		String input = "";
+		boolean again = false;
+		do{
+			System.out.print("please, enter the " + BEDay +" day. e.g.(2000/03/01): ");
+			input = userInput.nextLine();
+			input = input.trim();
+			if(input.length() == 10 && input.substring(4,5).equals("/") && input.substring(7,8).equals("/")){
+				try {
+					//check the time is or not integer
+					int year = Integer.parseInt(input.substring(0, 4));
+					int month = Integer.parseInt(input.substring(5, 7));
+					int day = Integer.parseInt(input.substring(8, 10));
+					switch(month){
+					//the months have 31 days 
+					case 1: case 3: case 5:
+		            case 7: case 8: case 10:
+		            case 12:
+		            	if(day<1 || day>31){
+		            		again = true;
+		            		System.out.println("Error: " + month + " has only 31 days");
+		            	}
+		            	else{
+		            		again = false;
+		            	}
+						break;
+					case 2:
+						//check the year is common or leap year
+						if((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))){
+							//common year Feb has 29 days
+							if(day<1 ||day>29){
+								again = true;
+								System.out.println("Error: " + month+" has only 29 days.");
+							}
+							else{
+								again = false;
+							}
+						}
+						else{
+							//leap year Feb has 28 days
+							if(day<1 || day>28){
+								again = true;
+								System.out.println("Error: "+month+" has only 28 days.");
+							}
+							else{
+								again = false;
+							}
+						}
+						break;
+					//the months have 30 days
+					case 4: case 6:
+		            case 9: case 11:
+		            	if(day<1 || day>30){
+		            		again = true;
+		            		System.out.println("Error: " + month+" has only 30 days.");
+
+		            	}
+		            	else{        		
+		            		again = false;
+		            	}
+						break;
+					default:
+						again = true;
+						System.out.println("outor: Invalid month.");
+						break;
+					}
+				}catch (NumberFormatException e) {
+					again = true;
+					System.out.println("Error: Enter number for year, month an day.");
+
+				}
+			}
+			else{
+				again = true;
+				System.out.println("Errro: Enter a correct day.");
+			}
+			
+		}while(again);
+		input = input.replace("/","");
+		input = input.trim();
+		return input;
+	}
+}
---
+++ src/ics314/dezesseis/calendar/inter.java
@@ -0,0 +1,71 @@
+package ics314.dezesseis.calendar;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.io.FileOutputStream;
+import java.util.Date;
+import java.util.Scanner;
+
+public class inter {
+    public static void main(String[] args) {
+    	String summary = "";
+    	String location = "";
+    	String beginDay = "";
+    	String endDay = "";
+    	String beginTime ="";
+    	String endTime="";
+    	String place ="";
+    	String description ="";
+    	boolean done = false;
+    	String input = "";
+    	Scanner userInput =new Scanner(System.in);
+    	InputCheck inputCheck = new InputCheck();
+        VObject calendar = new VObject(VObjectType.CALENDAR);
+        VObject event = new VObject(VObjectType.EVENT);
+        do{
+        System.out.print("Hello, do you want to create any Goolge calendar event,yes or no? y/n : ");
+        input = userInput.nextLine(); 
+        }while(!input.equalsIgnoreCase("y") && !input.equalsIgnoreCase("n"));    	
+        //
+        if(input.equalsIgnoreCase("n"))
+        	System.exit(0);
+
+        System.out.print("Please, Enter a title of the event: ");
+       	summary = userInput.nextLine();
+       	summary = summary.trim();
+       	beginDay = inputCheck.CheckDate("beginning")+"T";
+       	beginTime = inputCheck.Checktime("beginning")+"00";
+       	endDay = inputCheck.CheckDate("ending")+"T";
+       	endTime= inputCheck.Checktime("ending")+"00";
+       	System.out.print("Please, enter the location of the event:");
+       	place = userInput.nextLine();
+       	place = place.trim();
+       	System.out.println("Please, enter a description of the event:");
+       	System.out.print("->");
+       	description = userInput.nextLine();
+       	description = description.trim();
+       	
+       	//date stamp;
+       	Date myDate = new Date();
+       	SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmss");
+       	String stamp = sdf.format(myDate);        	
+       	stamp = stamp.replace("-", "T");
+       
+       	event.addSummary(summary);
+       	event.addDescription(description);
+       	event.addDtStart(beginDay + beginTime);
+       	event.addDtEnd(endDay + endTime);
+       	event.addDtStamp(stamp);
+       	event.addLocation(place);
+       	calendar.addChild(event);
+       //need to change a the dir
+       	File out = new File("/Users/NikoBenick/Desktop/test.ics");
+       	try(FileOutputStream outStream = new FileOutputStream(out)) {
+       		outStream.write(calendar.getTextRepresentation().getBytes());
+       		outStream.close();
+       	} catch (IOException e) {
+       		e.printStackTrace();
+       	}
+      }
+}
