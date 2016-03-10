bbed903ed2c7adfb85559e24f68f4256f8ec0eab
ZaneWoodard <zwoodard@hawaii.edu>
2016-03-10 13:06:41 -1000

    Add more complete error handling

--- src/ics314/dezesseis/calendar/GEO.java
+++ src/ics314/dezesseis/calendar/GEO.java
@@ -2,80 +2,82 @@ package ics314.dezesseis.calendar;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLEncoder;
 
 
 public class GEO {
-	private String location;
-	private String output;
-	
-	//structure
-	public GEO(String location){
-		this.location = location;
-		this.output="";
-	}
-	
-	
-	/*********************
-	 * connect to google map, send the address to the map to find the position
-	 * @param address
-	 * @return a String of "latitude;longitude" or "ERROR" google map cannot location the position;
-	 * @throws Exception
-	 **********************/
-	private static String getLatLongPositions(String address) throws Exception
-	  {
-	    int responseCode = 0;
-	    String api = "http://maps.googleapis.com/maps/api/geocode/xml?address=" + URLEncoder.encode(address, "UTF-8") + "&sensor=true";
-	    URL url = new URL(api);
-	    HttpURLConnection httpConnection = (HttpURLConnection)url.openConnection();
-	    httpConnection.connect();
-	    //HTTP Status Codes value= 200 -> OK;
-	    responseCode = httpConnection.getResponseCode();
-	    if(responseCode == 200)
-	    {
-	      //httpConnection.getinputstream return a XML file 
-	      BufferedReader stdout = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
-	      String line;
+    private String location;
+    private String output;
 
-          while((line = stdout.readLine())!= null){
-        	 // System.out.println(line);
-        	  if(line.indexOf("location") != -1){
-        		 String latitude = stdout.readLine();
-        		 latitude = latitude.replace("<lat>", "");
-        		 latitude = latitude.replace("</lat>", "");
-        		 String longitude = stdout.readLine();
-        		 longitude = longitude.replace("<lng>", "");
-        		 longitude = longitude.replace("</lng>", "");
-        		 return latitude +";"+ longitude.trim();
-        	 	}
-        	  
-          }
-          System.out.println("ERROR: cannot connect to internet.");
-	    }
-	    return "ERROR";
-	  }
-	
-	public void setPosition(String position){
-		output =position;
-		
-	}
-	
-	/************************
-	 * return the position
-	 * @return .ics format position of location or ERROR for not found
-	 */
-	public String getPosition() {
-		try {
-			
-			output = getLatLongPositions(location);
-			if(output.equals("ERROR"))
-				return "ERROR";
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println("ERROR");
-		}
-		return output.trim();
-	}
-		
+    //structure
+    public GEO(String location){
+        this.location = location;
+        this.output="";
+    }
+
+
+    /*********************
+     * connect to google map, send the address to the map to find the position
+     * @param address
+     * @return a String of "latitude;longitude" or "ERROR" google map cannot location the position;
+     * @throws Exception
+     **********************/
+    private static String getLatLongPositions(String address) throws Exception
+    {
+        int responseCode = 0;
+        String api = "http://maps.googleapis.com/maps/api/geocode/xml?address=" + URLEncoder.encode(address, "UTF-8") + "&sensor=true";
+        URL url = new URL(api);
+        HttpURLConnection httpConnection = (HttpURLConnection)url.openConnection();        
+        httpConnection.setConnectTimeout(5*1000);
+        httpConnection.connect();
+        //HTTP Status Codes value= 200 -> OK;
+        responseCode = httpConnection.getResponseCode();
+        if(responseCode == 200)
+        {
+            //httpConnection.getinputstream return a XML file 
+            BufferedReader stdout = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
+            String line;
+
+            while((line = stdout.readLine())!= null){
+                // System.out.println(line);
+                if(line.indexOf("location") != -1){
+                    String latitude = stdout.readLine();
+                    latitude = latitude.replace("<lat>", "");
+                    latitude = latitude.replace("</lat>", "");
+                    String longitude = stdout.readLine();
+                    longitude = longitude.replace("<lng>", "");
+                    longitude = longitude.replace("</lng>", "");
+                    return latitude +";"+ longitude.trim();
+                }
+
+            }
+        }
+        return "ERROR";
+    }
+
+    public void setPosition(String position){
+        output =position;
+
+    }
+
+    /************************
+     * return the position
+     * @return .ics format position of location or ERROR for not found
+     */
+    public String getPosition() {
+        try {
+            output = getLatLongPositions(location);
+            return output.trim();
+        } catch (SocketTimeoutException e) {
+            //Exception occurs when couldn't connect
+            System.out.println("ERROR: cannot connect to Google.");
+        } catch(Exception e) {
+            //Unexpected error, print it to make sure we know about it
+            e.printStackTrace();
+        }
+        return "ERROR";
+    }
+
 }
