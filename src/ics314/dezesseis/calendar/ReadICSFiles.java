cace5494b68e79d63bb05980fae1ccf6dafd54bc
Zane Woodard <zane.kt.woodard@gmail.com>
2016-04-07 23:01:46 -1000

    Add interface component to prompt for and read in a file containing events

---
+++ src/ics314/dezesseis/calendar/ReadICSFiles.java
@@ -0,0 +1,45 @@
+package ics314.dezesseis.calendar;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReadICSFiles {
+
+    public static List<VObject> start() {
+        Scanner cliReader = new Scanner(System.in);
+
+        boolean validFile = false;
+        File icsFileIn = null;
+        while(!validFile) {
+            System.out.print("Please enter name of file to read: ");
+            String fileName = cliReader.nextLine();
+
+            if(fileName.endsWith(".ics")) {
+                System.out.println("Error! File not a .ics file!");
+            } else if((icsFileIn=new File(fileName)).exists()) {
+                System.out.println("Error! File does not exist!");
+            } else if(icsFileIn.isDirectory()) {
+                System.out.println("Error! Path is a directory not a file!");
+            } else if(!icsFileIn.canRead() || !icsFileIn.canWrite()) {
+                System.out.println("Error! Cannot read and/or write to the file!");
+            } else {
+                validFile = true;
+            }
+        }
+
+        Reader icsFileReader = new Reader();
+        try {
+            icsFileReader.readFile(icsFileIn.getAbsolutePath());
+        } catch(IOException error) {
+            System.out.println("An unknown error occured when reading the calendar file!");
+            System.out.println("Please report the following to the developers:");
+            error.printStackTrace();
+        }
+
+        return icsFileReader.getList();
+
+    }
+
+}
