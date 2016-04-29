dabd1aade07021eb301b2c6dbb9e7b768645f7ba
niko925 <niko2012925@gmail.com>
2016-04-28 15:20:13 -1000

    Added files via upload

--- src/ics314/dezesseis/calendar/interfacecomponents/ReadICSFiles.java
+++ src/ics314/dezesseis/calendar/interfacecomponents/ReadICSFiles.java
@@ -45,4 +45,38 @@ public class ReadICSFiles {
 
     }
 
+    public static List<VObject> test() {
+        //Scanner cliReader = new Scanner(System.in);
+
+        boolean validFile = false;
+        File icsFileIn = null;
+        while(!validFile) {
+            System.out.print("Please enter name of file to read: ");
+            String fileName = System.getProperty("user.dir")+"/src/ics314/dezesseis/calendar/resources/gcd_demo.ics";
+
+            if(!fileName.endsWith(".ics")) {
+                System.out.println("Error! File not a .ics file!");
+            } else if(!(icsFileIn=new File(fileName)).exists()) {
+                System.out.println("Error! File does not exist!");
+            } else if(icsFileIn.isDirectory()) {
+                System.out.println("Error! Path is a directory not a file!");
+            } else if(!icsFileIn.canRead() || !icsFileIn.canWrite()) {
+                System.out.println("Error! Cannot read and/or write to the file!");
+            } else {
+                validFile = true;
+            }
+        }
+
+        Reader icsFileReader = new Reader();
+        try {
+            icsFileReader.readFile(icsFileIn.getAbsolutePath());
+        } catch(IOException error) {
+            System.out.println("An unknown error occured when reading the calendar file!");
+            System.out.println("Please report the following to the developers:");
+            error.printStackTrace();
+        }
+
+        return icsFileReader.getList();
+
+    }
 }
